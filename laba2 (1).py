0421bcb5bc8b32f288aa168b2a727ee93617b896
MihayloSumaruk <misha30sumaruk@gmail.com>
2024-09-11 13:45:26 +0300

    Add files via upload

---
+++ laba2 (1).py
@@ -0,0 +1,12 @@
+a = "Михайло"
+b = "Сумарук"
+c = 16
+list1=[a, b, c]
+print(list1)
+list2=[type(a), type(b), type(c)]
+print(list2)
+if type(a) == type(b) and type(b) == type(c) and type(a) == type(c):
+    print("Тип str, повторюється найчастіше")
+else:
+    print("Тип даних незбігаються")
+    
