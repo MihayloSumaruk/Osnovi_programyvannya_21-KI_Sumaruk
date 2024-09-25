5981646e023da9e56b1a49eef2a932b4812096f3
MihayloSumaruk <misha30sumaruk@gmail.com>
2024-09-25 09:57:35 +0300

    Update laba2 (1).py

--- laba2 (1).py
+++ laba2 (1).py
@@ -1,11 +1,30 @@
-a  = "Михайло"
-b = "Сумарук"
-c = 16
-list1=[a, b, c]
-print(list1)
-list2=[type(a), type(b), type(c)]
-print(list2)
-if type(a) == type(b) and type(b) == type(c) and type(a) == type(c):
-    print("Тип:", type(a), "повторюється найчастіше")
-else:
-    print("Типи даних незбігаються")
+a = 'Сумарук' 
+b = 'Михайло' 
+c = 16 
+g = type(a) 
+k = type(b) 
+h = type(c) 
+list1 = [a, b, c] 
+list2 = [g, k, h ] 
+list_string = [] 
+list_int = [] 
+ 
+if g == str: 
+    list_string.append(g) 
+elif g == int: 
+    list_int.append(g) 
+ 
+if k == str: 
+    list_string.append(k) 
+elif k == int: 
+    list_int.append(k)   
+ 
+if h == str: 
+    list_string.append(h) 
+elif h == int: 
+    list_int.append(h) 
+ 
+if len(list_string) > len(list_int): 
+    print('Тип даних str переважає') 
+elif len(list_int) > len(list_string): 
+    print('Тип даних int переважає')
