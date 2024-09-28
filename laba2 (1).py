779b210c7f533e4538fdccbee9681ff13174b3e0
MihayloSumaruk <misha30sumaruk@gmail.com>
2024-09-28 19:34:03 +0300

    Update laba2 (1).py

--- laba2 (1).py
+++ laba2 (1).py
@@ -3,4 +3,4 @@ types = [type(value) for value in list1]
 from collections import Counter
 type_counter = Counter(types)
 most_common_type = type_counter.most_common(1)[0][0]
-print(f"Найчастіше зустрічаючийся тип даних: {most_common_type}")
+print("Найчастіше зустрічаючийся тип даних:", {most_common_type})
