80031cb1f0cfd2e3b443779605c7a391574143ed
MihayloSumaruk <misha30sumaruk@gmail.com>
2024-09-28 19:32:45 +0300

    Update laba2 (1).py

--- laba2 (1).py
+++ laba2 (1).py
@@ -1,30 +1,6 @@
-a = 'Сумарук' 
-b = 'Михайло' 
-c = 16 
-g = type(a) 
-k = type(b) 
-h = type(c) 
-list1 = [a, b, c] 
-list2 = [g, k, h ] 
-list_string = [] 
-list_int = [] 
- 
-if g == str: 
-    list_string.append(g) 
-elif g == int: 
-    list_int.append(g) 
- 
-if k == str: 
-    list_string.append(k) 
-elif k == int: 
-    list_int.append(k)   
- 
-if h == str: 
-    list_string.append(h) 
-elif h == int: 
-    list_int.append(h) 
- 
-if len(list_string) > len(list_int): 
-    print('Тип даних str переважає') 
-elif len(list_int) > len(list_string): 
-    print('Тип даних int переважає')
+list1 = ["asdasda", 123, 12455, "asdaggfghs", "35gg", 1245]
+types = [type(value) for value in list1]
+from collections import Counter
+type_counter = Counter(types)
+most_common_type = type_counter.most_common(1)[0][0]
+print(f"Найчастіше зустрічаючийся тип даних: {most_common_type}")
