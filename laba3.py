0d0b8c871c2b6a07ddf5cb2674e6a57e1097be9a
MihayloSumaruk <misha30sumaruk@gmail.com>
2024-10-02 17:59:26 +0300

    Update laba3.py

--- laba3.py
+++ laba3.py
@@ -3,5 +3,14 @@ dict1 = {1:"Python",
          3:{3.1: "Pascal", 3.2: "Assembler",3.3: "C#",3.4: "Java", 3.5: "Visual Basic"},
          4: "SQL"}
 print(dict1)
-dict2 = {1: type(dict1[1]), 2: type(dict1[2]), 3: type(dict1[3]), 4: type(dict1[4])}
-print(dict2)
+
+dict_types = {}
+for key in dict1:
+    val = dict1[key]
+    if type(val) == dict:
+        for sub_key in val:
+            sub_val = type(val[sub_key])
+            dict_types[sub_key] = type[sub_val]
+    else:
+        dict_types[key] = type(val)
+print(dict_types)
