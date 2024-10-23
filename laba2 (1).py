08f4e1419998f7655120f5bf348f29d8fd55ee83
MihayloSumaruk <misha30sumaruk@gmail.com>
2024-10-23 10:54:18 +0300

    Update laba2 (1).py

--- laba2 (1).py
+++ laba2 (1).py
@@ -1,5 +1,4 @@
-x = {2,45,3,53,"asda"}
-c = [type(value) for value in x]
+x = [2,45,3,53,"asda"]
 int_list = []
 str_list = []
 float_list = []
@@ -15,4 +14,4 @@ for z in x:
         bool_list.append(z)
 d = { "int": len(int_list),"str": len(str_list),"float": len(float_list),"bool": len(bool_list)}
 v =max(d, key = d.get)
-print(f"Найбільше елементів у списку з типом даних: {v}")
+print(f"Найбільше елементів у списку з типом даних:", v)
